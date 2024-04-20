2c9ffc84dfd499f36943623b929d0d014e33d82e
luxq <luxq@luxqs-MacBook-Pro.local>
2024-04-20 17:50:27 +0800

    update script

--- conf/gen.sh
+++ conf/gen.sh
@@ -2,14 +2,17 @@
 
 NodeList=("node1" "node2" "node3" "node4")
 
-rm -f ca.crt ca.csr ca.key ca.srl > /dev/null 2>&1
+rm -rf ca > /dev/null 2>&1
+
+cadir="./ca"
+mkdir $cadir
 
 # 1. gen ca.key
-openssl genrsa -out ca.key 4096
+openssl genrsa -out ${cadir}/ca.key 4096
 # 2. gen ca.csr
-openssl req -new -sha256 -out ca.csr -key ca.key -config ca.conf
+openssl req -new -sha256 -out ${cadir}/ca.csr -key ${cadir}/ca.key -config ca.conf
 # 3. gen ca.crt
-openssl x509 -req -days 3650 -in ca.csr -signkey ca.key -out ca.crt
+openssl x509 -req -days 3650 -in ${cadir}/ca.csr -signkey ${cadir}/ca.key -out ${cadir}/ca.crt
 
 for element in ${NodeList[*]}
 do
@@ -24,7 +27,7 @@ do
   openssl req -new -sha256 -out ${element}/server.csr -key ${element}/server.key -config ${element}/server.conf
 
   # 3. gen server.pem with ca.crt and ca.key
-  openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial -days 365 -in ${element}/server.csr -out ${element}/server.crt -extensions req_ext -extfile ${element}/server.conf
+  openssl x509 -req -sha256 -CA ${cadir}/ca.crt -CAkey ${cadir}/ca.key -CAcreateserial -days 365 -in ${element}/server.csr -out ${element}/server.crt -extensions req_ext -extfile ${element}/server.conf
 
   # client side
   # 1. gen client.key
@@ -34,6 +37,6 @@ do
   openssl req -new -key ${element}/client.key -out ${element}/client.csr
 
   # 3. gen client.crt with ca and ca.key
-  openssl x509 -req -sha256 -CA ca.crt -CAkey ca.key -CAcreateserial -days 365  -in ${element}/client.csr -out ${element}/client.crt
+  openssl x509 -req -sha256 -CA ${cadir}/ca.crt -CAkey ${cadir}/ca.key -CAcreateserial -days 365  -in ${element}/client.csr -out ${element}/client.crt
 
 done
