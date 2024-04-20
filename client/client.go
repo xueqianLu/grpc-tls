163fd7064de5c5060ee4a92e9a0f3f03d97c0389
luxq <luxq@luxqs-MacBook-Pro.local>
2024-04-20 21:51:37 +0800

    update client and server

--- client/client.go
+++ client/client.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"grpc-tls/tlstool"
 	"log"
+	"math/rand"
+	"strings"
 	"time"
 
 	"google.golang.org/grpc"
 	pb "grpc-tls/helloworld"
 )
 
-func main() {
-	c, err := tlstool.GetClientTlsConfig("node1", "node1")
+var (
+	serverList = flag.String("server_list", "node1", "server list")
+)
+
+func todoFunc(clientid string, server string, seridx int) {
+	basePort := 9000
+	port := basePort + seridx
+
+	c, err := tlstool.GetClientTlsConfig(clientid, server)
 	if err != nil {
 		log.Fatalf("get client tls config err: %v", err)
 	}
 
 	// create client connection
 	conn, err := grpc.Dial(
-		"0.0.0.0:9000",
+		fmt.Sprintf("0.0.0.0:%d", port),
 		grpc.WithTransportCredentials(c),
 	)
 	if err != nil {
@@ -32,9 +43,27 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	resp, err := client.SayHello(ctx, &pb.HelloRequest{Name: "Sager"})
+	resp, err := client.SayHello(ctx, &pb.HelloRequest{Name: server})
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Greeting: %s", resp.GetMessage())
+
+}
+
+func main() {
+	flag.Parse()
+	sList := strings.Split(*serverList, ",")
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			cidx := rand.Intn(len(sList))
+			sidx := rand.Intn(len(sList))
+			todoFunc(sList[cidx], sList[sidx], sidx)
+		}
+	}
+
 }
--- server/server.go
+++ server/server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"grpc-tls/tlstool"
 	"log"
 	"net"
+	"strings"
 
 	"google.golang.org/grpc"
 	pb "grpc-tls/helloworld"
@@ -19,27 +22,43 @@ func (s *greeterService) SayHello(ctx context.Context, request *pb.HelloRequest)
 	return &pb.HelloReply{Message: "Hello " + request.GetName()}, nil
 }
 
+var (
+	serverList = flag.String("server_list", "node1", "server list")
+)
+
 func main() {
-	// 公钥中读取和解析公钥/私钥对
-	cred, err := tlstool.GetServerTlsConfig("node.chain.app")
-	if err != nil {
-		log.Fatalf("get server tls config err: %v", err)
-	}
-	// create grpc server
-	grpcServer := grpc.NewServer(grpc.Creds(cred))
-	// listen port
-	lis, err := net.Listen("tcp", "0.0.0.0:9000")
-	if err != nil {
-		log.Fatalf("list port err: %v", err)
+	flag.Parse()
+	basePort := 9000
+	sList := strings.Split(*serverList, ",")
+	for idx, s := range sList {
+		port := basePort + idx
+
+		// 公钥中读取和解析公钥/私钥对
+		cred, err := tlstool.GetServerTlsConfig(s)
+		if err != nil {
+			log.Printf("get server tls for (%s) err: %v", s, err)
+			continue
+		}
+		// create grpc server
+		grpcServer := grpc.NewServer(grpc.Creds(cred))
+		// listen port
+		lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+		if err != nil {
+			log.Printf("listen port err: %v", err)
+			continue
+		}
+
+		// register service into grpc server
+		pb.RegisterGreeterServiceServer(grpcServer, &greeterService{})
+		log.Printf("server: %v at port %d\n", s, port)
+
+		// listen port
+		go func() {
+			if err := grpcServer.Serve(lis); err != nil {
+				log.Printf("grpc serve err: %v", err)
+			}
+		}()
 	}
 
-	// register service into grpc server
-	pb.RegisterGreeterServiceServer(grpcServer, &greeterService{})
-
-	log.Printf("listening at %v", lis.Addr())
-
-	// listen port
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("grpc serve err: %v", err)
-	}
+	<-make(chan struct{})
 }
